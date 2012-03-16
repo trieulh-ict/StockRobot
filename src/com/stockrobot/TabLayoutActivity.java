66108d88fbf14c2b091d0435aaa62c78aea96eaa
Le Hai Trieu <trieulh.bk@gmail.com>
2012-03-16 11:14:04 +0700

    Change file TabLayout

--- src/com/stockrobot/TabLayoutActivity.java
+++ src/com/stockrobot/TabLayoutActivity.java
@@ -7,35 +7,39 @@ import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
-public class TabLayoutActivity extends TabActivity{
+public class TabLayoutActivity extends TabActivity {
 
-	public void onCreate(Bundle savedInstanceState){
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab_layout);
-		
+
 		TabHost tabHost = getTabHost();
-		
-		//Day list
+
+		// Day list
 		TabSpec day_tab = tabHost.newTabSpec("Day");
-		day_tab.setIndicator("Day", getResources().getDrawable(R.drawable.icon_tab));
+		day_tab.setIndicator("Day",
+				getResources().getDrawable(R.drawable.icon_tab));
 		Intent daysIntent = new Intent(this, DayListActivity.class);
 		day_tab.setContent(daysIntent);
-		
-		//Month list
+
+		// Month list
 		TabSpec month_tab = tabHost.newTabSpec("Month");
-		month_tab.setIndicator("Month", getResources().getDrawable(R.drawable.icon_tab));
+		month_tab.setIndicator("Month",
+				getResources().getDrawable(R.drawable.icon_tab));
 		Intent monthsIntent = new Intent(this, MonthListActivity.class);
 		month_tab.setContent(monthsIntent);
-		
-		//Year list
+
+		// Year list
 		TabSpec year_tab = tabHost.newTabSpec("Year");
-		year_tab.setIndicator("Year", getResources().getDrawable(R.drawable.icon_tab));
+		year_tab.setIndicator("Year",
+				getResources().getDrawable(R.drawable.icon_tab));
 		Intent yearsIntent = new Intent(this, YearListActivity.class);
 		year_tab.setContent(yearsIntent);
-		
+
 		tabHost.addTab(day_tab);
 		tabHost.addTab(month_tab);
 		tabHost.addTab(year_tab);
-		
-	}	
+		System.out.println();
+	}
 }
